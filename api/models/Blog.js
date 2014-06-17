bfcebd7dcae5fd03b5570da122f53154c30f4efb
Kevin <kk415kk@gmail.com>
2014-06-16 21:45:44 -0700

    added blogcontroller, more pages, updated github functionality

---
+++ api/models/Blog.js
@@ -0,0 +1,26 @@
+/**
+ * Blog
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+  	title: 'string',
+  	body: 'string',
+  	category: 'string',
+  	tags: 'string',
+  	date: 'string',
+  	author: 'string',
+  	type: 'string' 		// Draft
+    
+  }
+
+};
